603914b0e30ed854fc08d35e8d942f5ec169298f
hangbozhang <59976512+hangbozhang@users.noreply.github.com>
2024-03-29 19:20:34 -1000

    add status bar
    text for now.
    Later can be replace with real bars

---
+++ Assets/StatusManagement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StatusManagement : MonoBehaviour
+{
+    public static StatusManagement instance;
+    public TMP_Text HPText;
+    public TMP_Text EnergyText;
+    public TMP_Text ScoreText;
+    public TMP_Text PiecesText;
+
+    int HP = 10;
+    int Energy = 5;
+    int Score = 0;
+    int Pieces = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HPText.text = "HP: " + HP;
+        EnergyText.text = "Energy: " + Energy;
+        ScoreText.text = "Score: " + Score;
+        PiecesText.text = "Pieces: " + Pieces;
+        
+    }
+
+    public void AddHP(int value)
+    {
+        HP += value;
+        HPText.text = "HP: " + HP;
+    }
+
+    public void ReduceHP(int value)
+    {
+        HP -= value;
+        HPText.text = "HP: " + HP;
+    }
+
+    public void AddEnergy(int value)
+    {
+        Energy += value;
+        EnergyText.text = "Energy: " + Energy;
+    }
+
+    public void ReduceEnergy(int value)
+    {
+        Energy -= value;
+        EnergyText.text = "Energy: " + Energy;
+    }
+
+    public void AddScore(int value)
+    {
+        Score += value;
+        ScoreText.text = "Score: " + Score;
+    }
+
+    public void AddPieces(int value)
+    {
+        Pieces += value;
+        PiecesText.text = "Pieces: " + Pieces;
+    }
+}
