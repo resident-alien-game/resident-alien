64742903bccdc9a7688fd57a3134f1776c1d05fa
Mikhail Shkaralevich <mkaralevich8@gmail.com>
2024-04-19 20:09:04 -1000

    Add gun sound

--- Assets/Scripts/GunControl.cs
+++ Assets/Scripts/GunControl.cs
@@ -7,9 +7,16 @@ public class GunControl : MonoBehaviour
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
     public float bulletSpeed = 20f;
+    public AudioSource gunShot;
+
+    public void Start()
+    {
+        gunShot = GetComponent<AudioSource>();
+    }
 
     public void Shoot()
     {
+        gunShot.Play();
         var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
         bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
     }
